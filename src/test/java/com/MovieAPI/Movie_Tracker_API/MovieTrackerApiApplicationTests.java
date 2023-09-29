b14dbeb9e5b837bf224d700722b148c383dc5ce9
Luis Garcia <113415598+6arcia-Lui5@users.noreply.github.com>
2023-09-28 20:34:25 -0700

    Original files for first version of API

---
+++ src/test/java/com/MovieAPI/Movie_Tracker_API/MovieTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MovieAPI.Movie_Tracker_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
